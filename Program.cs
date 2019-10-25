f4380360e5621e932f1ef88de5b7198cdf1dfc17
Jamie Hamilton (P110103851) <P110103851@student.peterborough.ac.uk>
2019-10-25 13:45:16 +0100

    Init DotNetCore Console

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PairProgramming_Polymorphism
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
