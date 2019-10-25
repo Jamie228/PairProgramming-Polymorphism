3b7ea59a575c3565d1831b11834620fbee98f3e7
Jamie Hamilton (P110103851) <P110103851@student.peterborough.ac.uk>
2019-10-25 14:01:51 +0100

    Character class created

---
+++ Character.cs
@@ -0,0 +1,50 @@
+using System;
+
+namespace PairProgramming_Polymorphism
+{
+    class Character
+    {
+        int currentX;
+        int currentY;
+        string name;
+        int health;
+        int armour;
+        int damage;
+        char sprite;
+
+        public void MoveUp()
+        {
+            currentY += 1;
+        }
+
+        public void MoveDown()
+        {
+            currentY -= 1;
+        }
+
+        public void MoveLeft()
+        {
+            currentX -= 1;
+        }
+
+        public void MoveRight()
+        {
+            currentX += 1;
+        }
+
+        public void Update()
+        {
+
+        }
+
+        public void Attack()
+        {
+
+        }
+
+        public void Draw()
+        {
+
+        }
+    }
+}
