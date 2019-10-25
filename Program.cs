9acbba19c631ae82672494ffbb7aa0c8632ca803
Jamie Hamilton (P110103851) <P110103851@student.peterborough.ac.uk>
2019-10-25 14:05:12 +0100

    Added message

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace PairProgramming_Polymorphism
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Elliot!");
         }
     }
 }
